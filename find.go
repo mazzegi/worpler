3d2ce4072c79c13f713bfb5d6a01fca106b33ee1
Martin Gilles <martin.gilles@gundlach-automotive.com>
2022-02-13 12:38:14 +0100

    add find and match v2

--- find.go
+++ find.go
@@ -1,6 +1,9 @@
 package worpler
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 func match(word string, pattern string, exclude string, include string) bool {
 	rsword := []rune(word)
@@ -34,6 +37,84 @@ func Find(pattern string, exclude string, include string) []string {
 	var matches []string
 	for _, word := range wordlist {
 		if match(strings.ToLower(word), strings.ToLower(pattern), strings.ToLower(exclude), strings.ToLower(include)) {
+			matches = append(matches, word)
+		}
+	}
+	return matches
+}
+
+//
+func matchV2(word string, pattern string, exclude string) bool {
+	word = strings.ToLower(word)
+	exclude = strings.ToLower(exclude)
+
+	rsword := []rune(word)
+	rswordIncludeCheck := []rune(word)
+	rspattern := []rune(pattern)
+	if len(rsword) != len(rspattern) {
+		return false
+	}
+	if strings.ContainsAny(word, exclude) {
+		return false
+	}
+
+	indexOf := func(rs []rune, fr rune) int {
+		for i, r := range rs {
+			if r == fr {
+				return i
+			}
+		}
+		return -1
+	}
+
+	// first match exact positions
+	for i, r := range pattern {
+		if r == '*' {
+			// wildcard * matches any
+			continue
+		}
+		matchPos := unicode.IsUpper(r)
+		if !matchPos {
+			continue
+		}
+		if rsword[i] != unicode.ToLower(r) {
+			return false
+		}
+		// mark with uppercase char
+		rswordIncludeCheck[i] = r
+	}
+
+	// last match deviant positions
+	for i, r := range pattern {
+		if r == '*' {
+			// wildcard * matches any
+			continue
+		}
+		matchPos := unicode.IsUpper(r)
+		if matchPos {
+			continue
+		}
+		mi := indexOf(rswordIncludeCheck, r)
+		switch {
+		case mi < 0:
+			return false
+		case mi == i:
+			// found on same pos -> not match
+			return false
+		default:
+			rswordIncludeCheck[mi] = unicode.ToUpper(r)
+		}
+	}
+	return true
+}
+
+// Find finds all words matching pattern, exluding those, which contain chars of "exclude"
+// Uppercase letters in pattern means, that they are already at their designation position
+// Lowercase letters in pattern means, that they occur in the answer, but on a different position
+func FindV2(pattern string, exclude string) []string {
+	var matches []string
+	for _, word := range wordlistAnswers {
+		if matchV2(word, pattern, exclude) {
 			matches = append(matches, word)
 		}
 	}
